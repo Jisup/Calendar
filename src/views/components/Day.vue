731c810ee9dd1d5dac231490c40cf9da48756e52
Jisup <xrl0603@naver.com>
2021-12-17 02:26:17 +0900

    [FE] feat : 일자별 공고 모달 틀 생성

--- src/views/components/Day.vue
+++ src/views/components/Day.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="days">
     <div class="day-title">{{ day }}</div>
-    <div class="day-content">뭐가들어갈까요??</div>
+    <div class="day-content">
+      <recruit-ment></recruit-ment>
+    </div>
   </div>
 </template>
 <script>
 import "@/style/Day.scss";
+import RecruitMent from "./components/Recruitment.vue";
 export default {
   name: "day",
+  components: {
+    RecruitMent,
+  },
   props: {
     year: Number,
     month: Number,
---
+++ src/views/components/components/Recruitment.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="recruit-modal"></div>
+</template>
+<script>
+export default {
+  name: "recruitment",
+  setup() {},
+};
+</script>
+<style lang=""></style>

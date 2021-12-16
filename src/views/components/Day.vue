7b6772d204c509dcf9dc9f6901097df02e22b6f4
Jisup <xrl0603@naver.com>
2021-12-17 03:34:09 +0900

    [FE] feat : 일자별 공고 데이터 취합 및 정렬

--- src/views/components/Day.vue
+++ src/views/components/Day.vue
@@ -9,6 +9,7 @@
 <script>
 import "@/style/Day.scss";
 import RecruitMent from "./components/Recruitment.vue";
+import { reactive } from "@vue/reactivity";
 export default {
   name: "day",
   components: {
@@ -22,7 +23,42 @@ export default {
     end: Array,
   },
   setup(props) {
-    console.log(props);
+    const state = reactive({
+      recruit: [],
+    });
+
+    const setRecruit = (_data, _type) => {
+      var _temp = [];
+      _data.forEach((_item) => {
+        /* 
+          새롭게 json데이터를 만들어주지않고 직접적으로 json.state로 처리할경우
+          후순위에 적용한 데이터만 삽입된다. 
+        */
+        let _empty = {
+          id: _item.id,
+          name: _item.name,
+          content: _item.content,
+          image: _item.image,
+          start_time: _item.start_time,
+          end_time: _item.end_time,
+          state: _type,
+        };
+        _temp.push(_empty);
+      });
+      _temp = _temp.sort((o1, o2) => {
+        return o1.name < o2.name ? -1 : o1.name > o2.name ? 1 : 0;
+      });
+      _temp.forEach((_temp_item) => {
+        state.recruit.push(_temp_item);
+      });
+    };
+    setRecruit(props.start, "시");
+    setRecruit(props.end, "끝");
+
+    console.log(state.recruit);
+    return {
+      state,
+    };
   },
 };
 </script>

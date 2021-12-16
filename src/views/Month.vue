de0d5752b3c49f1ee810c580b5197a3c8f2cd2d3
Jisup <xrl0603@naver.com>
2021-12-16 15:47:35 +0900

    [FE] feat : 달력 상단 기본 틀 생성

--- src/views/Month.vue
+++ src/views/Month.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="calendar">여긴달력이 나왐</div>
+  <div class="calendar">
+    <div class="title">
+      <div class="prev-btn">&lt;</div>
+      <div class="date">2021.09</div>
+      <div class="next-btn">&gt;</div>
+    </div>
+    <div class="week">
+      <div class="day">일</div>
+      <div class="day">월</div>
+      <div class="day">화</div>
+      <div class="day">수</div>
+      <div class="day">목</div>
+      <div class="day">금</div>
+      <div class="day">토</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import "@/style/Month.css";
 export default {
   name: "month",
 };

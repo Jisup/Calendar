d53a55babd9d504a2b50cbb6de92234c73015ac9
Jisup <xrl0603@naver.com>
2021-12-16 16:42:04 +0900

    [FE] feat : 달력 하단 기본 틀 생성

--- src/views/Month.vue
+++ src/views/Month.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="calendar">
-    <div class="title">
-      <div class="prev-btn">&lt;</div>
-      <div class="date">2021.09</div>
-      <div class="next-btn">&gt;</div>
+    <div class="calendar-title">
+      <div class="calendar-title-prev-btn">&lt;</div>
+      <div class="calendar-title-date">2021.09</div>
+      <div class="calendar-title-next-btn">&gt;</div>
     </div>
-    <div class="week">
-      <div class="day">일</div>
-      <div class="day">월</div>
-      <div class="day">화</div>
-      <div class="day">수</div>
-      <div class="day">목</div>
-      <div class="day">금</div>
-      <div class="day">토</div>
+    <div class="month">
+      <div class="week">
+        <div class="day">일</div>
+        <div class="day">월</div>
+        <div class="day">화</div>
+        <div class="day">수</div>
+        <div class="day">목</div>
+        <div class="day">금</div>
+        <div class="day">토</div>
+      </div>
+      <div class="daylist">
+        <day-s v-for="(day, index) in 31" :key="index" :date="day"> </day-s>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import "@/style/Month.css";
+import DayS from "@/views/components/Day.vue";
 export default {
   name: "month",
+  components: {
+    DayS,
+  },
 };
 </script>
---
+++ src/views/components/Day.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="days">
+    <div class="day-title">{{ date }}</div>
+    <div class="day-content">뭐가들어갈까요??</div>
+  </div>
+</template>
+<script>
+import "@/style/Day.scss";
+export default {
+  name: "day",
+  props: {
+    date: Number,
+  },
+  setup(props) {
+    console.log(props.date);
+  },
+};
+</script>

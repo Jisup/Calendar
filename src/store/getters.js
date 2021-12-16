87e7d5ea1a1eaba39652b19ef58d6c7bea7e3d8a
Jisup <xrl0603@naver.com>
2021-12-16 17:08:25 +0900

    [FE] feat : 월별 및 시작-끝 데이터 저장 틀 생성

---
+++ src/store/getters.js
@@ -0,0 +1,36 @@
+export function getMonthJan(state) {
+  return state.Jan;
+}
+export function getMonthFeb(state) {
+  return state.Feb;
+}
+export function getMonthMar(state) {
+  return state.Mar;
+}
+export function getMonthApr(state) {
+  return state.Apr;
+}
+export function getMonthMay(state) {
+  return state.May;
+}
+export function getMonthJun(state) {
+  return state.Jun;
+}
+export function getMonthJul(state) {
+  return state.Jul;
+}
+export function getMonthAug(state) {
+  return state.Aug;
+}
+export function getMonthSep(state) {
+  return state.Sep;
+}
+export function getMonthOct(state) {
+  return state.Oct;
+}
+export function getMonthNov(state) {
+  return state.Nov;
+}
+export function getMonthDec(state) {
+  return state.Dec;
+}
---
+++ src/store/state.js
@@ -0,0 +1,50 @@
+export default {
+  Jan: {
+    start: [],
+    end: [],
+  },
+  Feb: {
+    start: [],
+    end: [],
+  },
+  Mar: {
+    start: [],
+    end: [],
+  },
+  Apr: {
+    start: [],
+    end: [],
+  },
+  May: {
+    start: [],
+    end: [],
+  },
+  Jun: {
+    start: [],
+    end: [],
+  },
+  Jul: {
+    start: [],
+    end: [],
+  },
+  Aug: {
+    start: [],
+    end: [],
+  },
+  Sep: {
+    start: [],
+    end: [],
+  },
+  Oct: {
+    start: [],
+    end: [],
+  },
+  Nov: {
+    start: [],
+    end: [],
+  },
+  Dec: {
+    start: [],
+    end: [],
+  },
+};

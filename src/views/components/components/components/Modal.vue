d06ed6fbd9e8c9341b7b48754679cd733ccf4e13
xrl0603 <xrl0603@naver.com>
2021-12-17 16:33:58 +0900

    [FE] feat : 모달 시간 표기

--- src/views/components/components/components/Modal.vue
+++ src/views/components/components/components/Modal.vue
@@ -13,8 +13,10 @@
         <div class="modal-top-right">
           <span>{{ modal_data.name }}</span>
           <div class="modal-date">
-            <span>{{ modal_data.start_time }}</span>
-            <span>{{ modal_data.end_time }}</span>
+            <span>{{ state.start_time[0] }} {{ state.start_time[1] }}</span>
+            <span> ~ </span>
+            <span>{{ state.end_time[0] }} {{ state.end_time[1] }}</span>
+            <span class="modal-date-during-time">{{ state.during_time }}</span>
           </div>
         </div>
       </div>
@@ -26,13 +28,52 @@
 </template>
 <script>
 import "@/style/Modal.css";
-import { onMounted } from "vue";
+import { useStore } from "vuex";
+import { reactive, onMounted } from "vue";
 export default {
   name: "modal",
   props: {
     modal_data: Object,
   },
   setup(props, { emit }) {
+    const store = useStore();
+    const state = reactive({
+      start_time: "",
+      end_time: "",
+      during_time: "",
+    });
+    state.start_time = props.modal_data.start_time.split("T");
+    state.start_time[1] = state.start_time[1].split(".")[0].slice(0, 5);
+    state.end_time = props.modal_data.end_time.split("T");
+    state.end_time[1] = state.end_time[1].split(".")[0].slice(0, 5);
+
+    const setModalTime = (_time) => {
+      let _year = _time.getFullYear();
+      let _month = _time.getMonth() + 1;
+      _month = _month < 10 ? "0" + _month : _month;
+      let _day = _time.getDate();
+      _day = _day < 10 ? "0" + _day : _day;
+      return new Date(_year + "-" + _month + "-" + _day).getTime();
+    };
+    var _end_time = new Date(props.modal_data.end_time);
+    _end_time = setModalTime(_end_time);
+    var _now_time = store.getters["root/getToday"];
+    _now_time = setModalTime(_now_time);
+
+    if (_end_time > _now_time) {
+      state.during_time =
+        "(" +
+        (Math.floor((_end_time - _now_time) / 1000 / 60 / 60 / 24) + 1) +
+        "일 전)";
+    } else if (_end_time < _now_time) {
+      state.during_time =
+        "(" +
+        Math.floor((_now_time - _end_time) / 1000 / 60 / 60 / 24) +
+        "일 지남)";
+    } else {
+      state.during_time = "(오늘)";
+    }
+
     onMounted(() => {
       var _modal_content = document.querySelector(".modal-content");
       _modal_content.innerHTML = props.modal_data.content;
@@ -40,7 +81,7 @@ export default {
     const closeModal = () => {
       emit("close");
     };
-    return { closeModal };
+    return { state, closeModal };
   },
 };
 </script>

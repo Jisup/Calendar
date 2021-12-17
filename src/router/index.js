d3e33c81d0f78edb68edcc3d52bedac0a1e4f557
Jisup <xrl0603@naver.com>
2021-12-17 10:17:26 +0900

    [FE] hotfix : 라우터 name 변환

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "month",
     component: () => import("@/views/Month.vue"),
   },
 ];

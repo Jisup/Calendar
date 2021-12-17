a08798607715e1be9b8d53d22a0e452df45a41f1
xrl0603 <xrl0603@naver.com>
2021-12-17 13:40:15 +0900

    [FE] fix : 모달 컨트롤 변수를 위한 기존 변수 수정

--- src/views/components/components/Recruitment.vue
+++ src/views/components/components/Recruitment.vue
@@ -1,5 +1,7 @@
 <template lang="">
-  <div class="recruit-title">{{ state }}-{{ name }}</div>
+  <div class="recruit-title" @click="openModal">
+    {{ state_type }}-{{ name }}
+  </div>
   <Modal
     v-model="state.modal"
     :id="id"
@@ -8,6 +10,8 @@
     :image="image"
     :start_time="start_time"
     :end_time="end_time"
+    :state_type="state_type"
+    @close="closeModal"
   ></Modal>
 </template>
 <script>
@@ -32,6 +36,17 @@ export default {
     const state = reactive({
       modal: false,
     });
+    const openModal = () => {
+      state.modal = true;
+    };
+    const closeModal = () => {
+      state.modal = false;
+    };
+    return {
+      state,
+      openModal,
+      closeModal,
+    };
   },
 };
 </script>
